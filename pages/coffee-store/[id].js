cd541865c7955e59aa56c2a17576905cad82c931
roluochKe <rasholuoch@gmail.com>
2022-01-03 15:30:30 +0300

    app architecture

---
+++ pages/coffee-store/[id].js
@@ -0,0 +1,9 @@
+import { useRouter } from 'next/router'
+
+const CoffeeStore = () => {
+  const router = useRouter()
+
+  return <div>Coffee Store Page Details</div>
+}
+
+export default CoffeeStore
